8049007517e0814b0bb4e138387aa2c9abf5b41b
ferdyt <example@gmail.com>
2026-03-08 10:33:08 +0200

    Add project files.

---
+++ Lab_1_part_B_asppr/InputManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_1_part_B_asppr
+{
+    internal static class InputManager
+    {
+        public static Matrix InputMatrix()
+        {
+            int rows, cols;
+            double[,] data;
+
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Введiть кiлькiсть рядкiв: ");
+                    rows = int.Parse(Console.ReadLine());
+                    Console.Write("Введiть кiлькiсть стовпцiв: ");
+                    cols = int.Parse(Console.ReadLine());
+                    data = new double[rows, cols];
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Помилка! Введено некоректне число. Спробуйте ще раз.");
+                    continue;
+                }
+            }
+
+            Console.WriteLine($"Введiть рядок матрицi:");
+            for (int i = 0; i < rows; i++)
+            {
+                while (true)
+                {
+                    Console.Write($"Рядок {i + 1}: ");
+                    string input = Console.ReadLine();
+                    string[] parts = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (parts.Length != cols)
+                    {
+                        Console.WriteLine($"Помилка! Ви ввели {parts.Length} чисел, а потрiбно {cols}. Спробуйте ще раз.");
+                        continue;
+                    }
+                    try
+                    {
+                        for (int j = 0; j < cols; j++)
+                        {
+                            data[i, j] = double.Parse(parts[j].Replace(',', '.'), System.Globalization.CultureInfo.InvariantCulture);
+                        }
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Помилка! Введено некоректне число. Спробуйте ще раз.");
+                    }
+                }
+            }
+            Matrix matrix = new Matrix(data);
+
+            return matrix;
+        }
+
+        public static double[] InputConstants()
+        {
+            int constNum;
+            double[] constants;
+
+            Console.Write("Введiть кiлькiсть констант: ");
+            while (true)
+            {
+                try
+                {
+                    constNum = int.Parse(Console.ReadLine());
+                    constants = new double[constNum];
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Помилка! Введено некоректне число. Спробуйте ще раз.");
+                    continue;
+                }
+            }
+            for (int i = 0; i < constNum; i++)
+            {
+                while (true)
+                {
+                    Console.Write($"Константа {i + 1}: ");
+                    string input = Console.ReadLine();
+                    try
+                    {
+                        constants[i] = double.Parse(input.Replace(',', '.'), System.Globalization.CultureInfo.InvariantCulture);
+                        break;
+                    }
+                    catch (FormatException)
+                    {
+                        Console.WriteLine("Помилка! Введено некоректне число. Спробуйте ще раз.");
+                    }
+                }
+            }
+
+            return constants;
+        }
+    }
+}
---
+++ Lab_1_part_B_asppr/Matrix.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_1_part_B_asppr
+{
+    internal class Matrix
+    {
+        private readonly double[,] _data;
+
+        public int Rows { get; }
+        public int Columns { get; }
+        public int Rank { get; set; }
+
+        public string[] RowHeaders { get; set; }
+        public string[] ColumnHeaders { get; set; }
+
+        public Matrix(double[,] matrix)
+        {
+            _data = matrix;
+            Rows = matrix.GetLength(0);
+            Columns = matrix.GetLength(1);
+        }
+
+        public double this[int row, int col]
+        {
+            get => _data[row, col];
+            set => _data[row, col] = value;
+        }
+
+        public Matrix Clone()
+        {
+            int rows = Rows;
+            int cols = Columns;
+            double[,] newData = new double[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+                for (int j = 0; j < cols; j++)
+                    newData[i, j] = this[i, j];
+
+            Matrix newMatrix = new Matrix(newData);
+
+            newMatrix.Rank = this.Rank;
+
+            if (this.RowHeaders != null)
+            {
+                newMatrix.RowHeaders = (string[])this.RowHeaders.Clone();
+            }
+
+            if (this.ColumnHeaders != null)
+            {
+                newMatrix.ColumnHeaders = (string[])this.ColumnHeaders.Clone();
+            }
+
+            return newMatrix;
+        }
+
+        public void InitializeHeaders()
+        {
+            RowHeaders = new string[Rows];
+            ColumnHeaders = new string[Columns];
+
+            for (int i = 0; i < Rows - 1; i++)
+            {
+                RowHeaders[i] = "y" + (i + 1);
+            }
+            RowHeaders[Rows - 1] = "Z";
+
+            for (int j = 0; j < Columns - 1; j++)
+            {
+                ColumnHeaders[j] = "x" + (j + 1);
+            }
+            ColumnHeaders[Columns - 1] = "1";
+        }
+    }
+}
